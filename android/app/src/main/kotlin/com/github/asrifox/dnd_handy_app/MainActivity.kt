cd2319e6d9c939aa0e91deed80c681773858400a
AsriFox <asrifox@yandex.ru>
2024-11-16 00:44:03 +0300

    Update Android build to Gradle 8.3

---
+++ android/app/src/main/kotlin/com/github/asrifox/dnd_handy_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.github.asrifox.dnd_handy_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/github/asrifox/dnd_handy_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.github.asrifox.dnd_handy_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
